361e0971f0fb5644e24a4523a94ac9f89ead6146
David Roman <d@vidroman.dev>
2021-09-03 17:28:21 +0100

    Initial commit

---
+++ Sources/FoundationEncore/DateIntervalFormatterEncore.swift
@@ -0,0 +1,13 @@
+extension DateIntervalFormatter {
+    public enum Format {
+        case preset(time: Style, date: Style)
+    }
+
+    public func setFormat(_ format: Format) {
+        switch format {
+        case .preset(let timeStyle, let dateStyle):
+            self.timeStyle = timeStyle
+            self.dateStyle = dateStyle
+        }
+    }
+}
