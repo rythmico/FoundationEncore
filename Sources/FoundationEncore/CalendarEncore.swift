47a4730fa573c2ad339deb14cee8d6752ff26bb2
davdroman <d@vidroman.dev>
2021-10-17 23:00:50 +0100

    Introduce `Calendar.init(identifier:locale:timeZone:)

--- Sources/FoundationEncore/CalendarEncore.swift
+++ Sources/FoundationEncore/CalendarEncore.swift
@@ -1,8 +1,11 @@
 extension Calendar {
     public static var neutral: Self {
-        Calendar(identifier: .iso8601) => {
-            $0.timeZone = .neutral
-            $0.locale = .neutral
-        }
+        Calendar(identifier: .iso8601, locale: .neutral, timeZone: .neutral)
+    }
+
+    public init(identifier: Identifier, locale: Locale, timeZone: TimeZone) {
+        self.init(identifier: identifier)
+        self.locale = locale
+        self.timeZone = timeZone
     }
 }
