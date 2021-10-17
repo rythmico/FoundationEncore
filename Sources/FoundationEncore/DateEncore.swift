f56cf098dcd08ec56138fa607351b7f1a6b55829
davdroman <d@vidroman.dev>
2021-10-18 00:05:16 +0100

    Fix date operations not respecting DST
    By enforcing an explicit time zone for each operation to take place in

--- Sources/FoundationEncore/DateEncore.swift
+++ Sources/FoundationEncore/DateEncore.swift
@@ -1,3 +1,12 @@
+extension DateComponents {
+    public func optionalValue(for unit: Calendar.Component) -> Int? {
+        guard let value = value(for: unit), value != NSNotFound else {
+            return nil
+        }
+        return value
+    }
+}
+
 extension Date {
     public static func => (lhs: Date, rhs: (set: Set<Calendar.Component>, to: Int, for: TimeZone)) -> Date {
         let (units, amount, timeZone) = rhs
@@ -9,20 +18,12 @@ extension Date {
         )
     }
 
-    public static func => (lhs: Date, rhs: (set: Set<Calendar.Component>, to: Int)) -> Date {
-        lhs => (set: rhs.set, to: rhs.to, for: .neutral)
-    }
-
     public static func => (lhs: Date, rhs: (set: Calendar.Component, to: Int, for: TimeZone)) -> Date {
         lhs => (set: [rhs.set], to: rhs.to, for: rhs.for)
     }
 
-    public static func => (lhs: Date, rhs: (set: Calendar.Component, to: Int)) -> Date {
-        lhs => (set: rhs.set, to: rhs.to, for: .neutral)
-    }
-
     private func setting(_ components: DateComponents, for timeZone: TimeZone) -> Date? {
-        let calendar = Calendar.neutral => (\.timeZone, timeZone)
+        let calendar = Self.calendar(for: timeZone)
         let allUnits: Set<Calendar.Component> = [.year, .month, .day, .hour, .minute, .second]
         let originalComponents = calendar.dateComponents(allUnits, from: self)
         let newComponents = allUnits.reduce(into: originalComponents) { acc, unit in
@@ -30,39 +31,35 @@ extension Date {
         }
         return calendar.date(from: newComponents)
     }
-}
 
-extension DateComponents {
-    public func optionalValue(for unit: Calendar.Component) -> Int? {
-        guard let value = value(for: unit), value != NSNotFound else {
-            return nil
-        }
-        return value
+    private static func calendar(for timeZone: TimeZone) -> Calendar {
+        Calendar(identifier: .iso8601, locale: .neutral, timeZone: timeZone)
     }
 }
 
 extension Date {
-    public static func + (lhs: Date, rhs: (amount: Int, unit: Calendar.Component)) -> Date {
-        Calendar.neutral.date(byAdding: rhs.unit, value: rhs.amount, to: lhs) !! preconditionFailure(
+    public static func + (lhs: Date, rhs: (amount: Int, unit: Calendar.Component, timeZone: TimeZone)) -> Date {
+        Self.calendar(for: rhs.timeZone).date(byAdding: rhs.unit, value: rhs.amount, to: lhs) !! preconditionFailure(
             "Date addition failed with 'date' \(lhs) 'amount' \(rhs.amount) 'unit' \(rhs.unit)"
         )
     }
 
-    public static func - (lhs: Date, rhs: (amount: Int, unit: Calendar.Component)) -> Date {
-        lhs + (-rhs.amount, rhs.unit)
+    public static func - (lhs: Date, rhs: (amount: Int, unit: Calendar.Component, timeZone: TimeZone)) -> Date {
+        lhs + (-rhs.amount, rhs.unit, rhs.timeZone)
     }
 
-    public static func - (lhs: Date, rhs: (date: Date, unit: Calendar.Component)) -> Int {
-        Calendar.neutral.dateComponents([rhs.unit], from: rhs.date, to: lhs).value(for: rhs.unit) !! preconditionFailure(
+    public static func - (lhs: Date, rhs: (date: Date, unit: Calendar.Component, timeZone: TimeZone)) -> Int {
+        Self.calendar(for: rhs.timeZone).dateComponents([rhs.unit], from: rhs.date, to: lhs).value(for: rhs.unit) !! preconditionFailure(
             "Date diff failed with 'toDate' \(lhs) 'fromDate' \(rhs.date) 'unit' \(rhs.unit)"
         )
     }
 }
 
 extension Date {
-    public init(date: Date, time: Date) {
-        let timeComponents = Calendar.neutral.dateComponents([.hour, .minute, .second], from: time)
-        self = date.setting(timeComponents, for: .neutral) !! preconditionFailure(
+    public init(date: Date, time: Date, timeZone: TimeZone) {
+        let calendar = Self.calendar(for: timeZone)
+        let timeComponents = calendar.dateComponents([.hour, .minute, .second], from: time)
+        self = date.setting(timeComponents, for: timeZone) !! preconditionFailure(
             "Date merging failed with 'date' \(date) 'time' \(time)"
         )
     }
