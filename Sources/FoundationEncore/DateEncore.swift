f28855341601a7474d173fdc8496e6ece6083da8
davdroman <d@vidroman.dev>
2021-10-19 21:34:29 +0100

    Make date operations failable

--- Sources/FoundationEncore/DateEncore.swift
+++ Sources/FoundationEncore/DateEncore.swift
@@ -7,29 +7,63 @@ extension DateComponents {
     }
 }
 
+public enum DateOperationError: LocalizedError {
+    case cannotMutate(Date, newComponents: DateComponents, TimeZone)
+    case cannotAdd(Date, amount: Int, unit: Calendar.Component, TimeZone)
+    case cannotDiff(minuend: Date, subtrahend: Date, unit: Calendar.Component, TimeZone)
+
+    public var errorDescription: String? {
+        switch self {
+        case .cannotMutate(let date, let components, let timeZone):
+            return """
+            Date mutation failed:
+            - Date: \(date)
+            - New Components: \(components)
+            - Time Zone: \(timeZone)
+            """
+        case .cannotAdd(let date, let amount, let unit, let timeZone):
+            return """
+            Date addition failed:
+            - Date: \(date)
+            - Amount: \(amount)
+            - Unit: \(unit)
+            - Time Zone: \(timeZone)
+            """
+        case .cannotDiff(let minuend, let subtrahend, let unit, let timeZone):
+            return """
+            Date diff failed:
+            - Minuend: \(minuend)
+            - Subtrahend: \(subtrahend)
+            - Unit: \(unit)
+            - Time Zone: \(timeZone)
+            """
+        }
+    }
+}
+
+// TODO: typed throws (when available)
+
 extension Date {
-    public static func => (lhs: Date, rhs: (set: Set<Calendar.Component>, to: Int, for: TimeZone)) -> Date {
+    public static func => (lhs: Date, rhs: (set: Set<Calendar.Component>, to: Int, for: TimeZone)) throws -> Date {
         let (units, amount, timeZone) = rhs
-        return lhs.setting(
-            rhs.set.reduce(into: DateComponents()) { $0.setValue(rhs.to, for: $1) },
+        return try lhs.setting(
+            units.reduce(into: DateComponents()) { $0.setValue(amount, for: $1) },
             for: timeZone
-        ) !! preconditionFailure(
-            "Date mutation failed with 'date' \(lhs) 'units' \(units) 'amount' \(amount) 'timeZone' \(timeZone)"
         )
     }
 
-    public static func => (lhs: Date, rhs: (set: Calendar.Component, to: Int, for: TimeZone)) -> Date {
-        lhs => (set: [rhs.set], to: rhs.to, for: rhs.for)
+    public static func => (lhs: Date, rhs: (set: Calendar.Component, to: Int, for: TimeZone)) throws -> Date {
+        try lhs => (set: [rhs.set], to: rhs.to, for: rhs.for)
     }
 
-    private func setting(_ components: DateComponents, for timeZone: TimeZone) -> Date? {
+    private func setting(_ components: DateComponents, for timeZone: TimeZone) throws -> Date {
         let calendar = Self.calendar(for: timeZone)
         let allUnits: Set<Calendar.Component> = [.year, .month, .day, .hour, .minute, .second]
         let originalComponents = calendar.dateComponents(allUnits, from: self)
         let newComponents = allUnits.reduce(into: originalComponents) { acc, unit in
             acc.setValue(components.optionalValue(for: unit) ?? acc.optionalValue(for: unit), for: unit)
         }
-        return calendar.date(from: newComponents)
+        return try calendar.date(from: newComponents) ?! DateOperationError.cannotMutate(self, newComponents: components, timeZone)
     }
 
     private static func calendar(for timeZone: TimeZone) -> Calendar {
@@ -40,20 +74,20 @@ extension Date {
 }
 
 extension Date {
-    public static func + (lhs: Date, rhs: (amount: Int, unit: Calendar.Component, timeZone: TimeZone)) -> Date {
-        Self.calendar(for: rhs.timeZone).date(byAdding: rhs.unit, value: rhs.amount, to: lhs) !! preconditionFailure(
-            "Date addition failed with 'date' \(lhs) 'amount' \(rhs.amount) 'unit' \(rhs.unit)"
-        )
+    public static func + (lhs: Date, rhs: (amount: Int, unit: Calendar.Component, timeZone: TimeZone)) throws -> Date {
+        try Self.calendar(for: rhs.timeZone).date(byAdding: rhs.unit, value: rhs.amount, to: lhs) ?! {
+            DateOperationError.cannotAdd(lhs, amount: rhs.amount, unit: rhs.unit, rhs.timeZone)
+        }()
     }
 
-    public static func - (lhs: Date, rhs: (amount: Int, unit: Calendar.Component, timeZone: TimeZone)) -> Date {
-        lhs + (-rhs.amount, rhs.unit, rhs.timeZone)
+    public static func - (lhs: Date, rhs: (amount: Int, unit: Calendar.Component, timeZone: TimeZone)) throws -> Date {
+        try lhs + (-rhs.amount, rhs.unit, rhs.timeZone)
     }
 
-    public static func - (lhs: Date, rhs: (date: Date, unit: Calendar.Component, timeZone: TimeZone)) -> Int {
-        Self.calendar(for: rhs.timeZone).dateComponents([rhs.unit], from: rhs.date, to: lhs).value(for: rhs.unit) !! preconditionFailure(
-            "Date diff failed with 'toDate' \(lhs) 'fromDate' \(rhs.date) 'unit' \(rhs.unit)"
-        )
+    public static func - (lhs: Date, rhs: (date: Date, unit: Calendar.Component, timeZone: TimeZone)) throws -> Int {
+        try Self.calendar(for: rhs.timeZone).dateComponents([rhs.unit], from: rhs.date, to: lhs).value(for: rhs.unit) ?! {
+            DateOperationError.cannotDiff(minuend: lhs, subtrahend: rhs.date, unit: rhs.unit, rhs.timeZone)
+        }()
     }
 }
 
@@ -61,7 +95,7 @@ extension Date {
     public init(date: Date, time: Date, timeZone: TimeZone) {
         let calendar = Self.calendar(for: timeZone)
         let timeComponents = calendar.dateComponents([.hour, .minute, .second], from: time)
-        self = date.setting(timeComponents, for: timeZone) !! preconditionFailure(
+        self = (try? date.setting(timeComponents, for: timeZone)) !! preconditionFailure(
             "Date merging failed with 'date' \(date) 'time' \(time)"
         )
     }
