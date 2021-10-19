554793461abe698b72f65aa73687c36a05c40b9a
davdroman <d@vidroman.dev>
2021-10-19 19:38:49 +0100

    Gardening

--- Sources/NilGuardingOperator/NilGuardingOperator.swift
+++ Sources/NilGuardingOperator/NilGuardingOperator.swift
@@ -1,16 +1,16 @@
-infix operator !! : NilCoalescingPrecedence
 infix operator ?! : NilCoalescingPrecedence
-
-public func !! <T>(optional: T?, exit: @autoclosure () -> Never) -> T {
-    guard let value = optional else {
-        exit()
-    }
-    return value
-}
+infix operator !! : NilCoalescingPrecedence
 
 public func ?! <T>(optional: T?, error: @autoclosure () -> Error) throws -> T {
     guard let value = optional else {
         throw error()
+    }
+    return value
+}
+
+public func !! <T>(optional: T?, exit: @autoclosure () -> Never) -> T {
+    guard let value = optional else {
+        exit()
     }
     return value
 }
