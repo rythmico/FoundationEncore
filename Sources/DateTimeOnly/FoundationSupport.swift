82e8d0dbfb4c2567d0b33fc34d2c1bb045e79246
davdroman <d@vidroman.dev>
2021-10-21 14:40:22 +0100

    Extract out `DateComponents.init(date:time:)`

--- Sources/DateTimeOnly/FoundationSupport.swift
+++ Sources/DateTimeOnly/FoundationSupport.swift
@@ -4,19 +4,25 @@ extension Date {
     public init(date: DateOnly, time: TimeOnly, timeZone: TimeZone) {
         var calendar = calendar
         calendar.timeZone = timeZone
-        let dateComponents = DateComponents(
-            calendar: calendar,
-            timeZone: timeZone,
+        var dateComponents = DateComponents(date: date, time: time)
+        dateComponents.calendar = calendar
+        dateComponents.timeZone = timeZone
+        guard let date = dateComponents.date else {
+            preconditionFailure("`DateComponents.date` returned nil")
+        }
+        self = date
+    }
+}
+
+extension DateComponents {
+    public init(date: DateOnly, time: TimeOnly) {
+        self.init(
             year: date.year,
             month: date.month,
             day: date.day,
             hour: time.hour,
             minute: time.minute
         )
-        guard let date = dateComponents.date else {
-            preconditionFailure("`DateComponents.date` returned nil")
-        }
-        self = date
     }
 }
 
