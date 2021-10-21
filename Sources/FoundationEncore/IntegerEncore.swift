57c9ee1cfbce3090e34ea58370ad1f90974b9463
davdroman <d@vidroman.dev>
2021-10-21 18:46:04 +0100

    Introduce integer overflow helpers

---
+++ Sources/FoundationEncore/IntegerEncore.swift
@@ -0,0 +1,61 @@
+extension BinaryInteger {
+    /// Equivalent to `BinaryInteger.init?(exactly:)`, just with a more obvious name.
+    public init?<T: BinaryInteger>(nilOnOverflow source: T) {
+        self.init(exactly: source)
+    }
+
+    /// Equivalent to `BinaryInteger.init?(exactly:)`, just with a more obvious name.
+    public init?<T: BinaryFloatingPoint>(nilOnOverflow source: T) {
+        self.init(exactly: source)
+    }
+}
+
+extension FixedWidthInteger {
+    public func addingOrNilOnOverflow(_ rhs: Self) -> Self? {
+        let (value, overflow) = self.addingReportingOverflow(rhs)
+        return overflow ? nil : value
+    }
+
+    public func dividedOrNilOnOverflow(by rhs: Self) -> Self? {
+        let (value, overflow) = self.dividedReportingOverflow(by: rhs)
+        return overflow ? nil : value
+    }
+
+    public func multipliedOrNilOnOverflow(by rhs: Self) -> Self? {
+        let (value, overflow) = self.multipliedReportingOverflow(by: rhs)
+        return overflow ? nil : value
+    }
+
+    public func remainderOrNilOnOverflow(dividingBy rhs: Self) -> Self? {
+        let (value, overflow) = self.remainderReportingOverflow(dividingBy: rhs)
+        return overflow ? nil : value
+    }
+
+    public func subtractingOrNilOnOverflow(_ rhs: Self) -> Self? {
+        let (value, overflow) = self.subtractingReportingOverflow(rhs)
+        return overflow ? nil : value
+    }
+}
+
+infix operator +? : AdditionPrecedence
+infix operator /? : MultiplicationPrecedence
+infix operator *? : MultiplicationPrecedence
+infix operator -? : AdditionPrecedence
+
+extension FixedWidthInteger {
+    public static func +? (lhs: Self, rhs: Self) -> Self? {
+        lhs.addingOrNilOnOverflow(rhs)
+    }
+
+    public static func /? (lhs: Self, rhs: Self) -> Self? {
+        lhs.dividedOrNilOnOverflow(by: rhs)
+    }
+
+    public static func *? (lhs: Self, rhs: Self) -> Self? {
+        lhs.multipliedOrNilOnOverflow(by: rhs)
+    }
+
+    public static func -? (lhs: Self, rhs: Self) -> Self? {
+        lhs.subtractingOrNilOnOverflow(rhs)
+    }
+}
---
+++ Tests/FoundationEncoreTests/IntEncoreTests.swift
@@ -0,0 +1,77 @@
+import FoundationEncore
+import XCTest
+
+final class IntegerEncoreTests: XCTestCase {
+    func testInitNilOnOverflow() {
+        XCTAssertEqual(Int(nilOnOverflow: UInt(0)), 0)
+        XCTAssertEqual(Int(nilOnOverflow: UInt(1000)), 1000)
+        XCTAssertEqual(Int(nilOnOverflow: UInt(Int.max) - 1), .max - 1)
+        XCTAssertEqual(Int(nilOnOverflow: UInt(Int.max)), .max)
+        XCTAssertEqual(Int(nilOnOverflow: UInt(Int.max) + 1), nil)
+        XCTAssertEqual(Int(nilOnOverflow: UInt.max), nil)
+    }
+
+    func testAddingOrNilOnOverflow() {
+        XCTAssertEqual(Int.max.addingOrNilOnOverflow(-2), Int.max - 2)
+        XCTAssertEqual(Int.max.addingOrNilOnOverflow(-1), Int.max - 1)
+        XCTAssertEqual(Int.max.addingOrNilOnOverflow(0), Int.max)
+        XCTAssertEqual(Int.max.addingOrNilOnOverflow(1), nil)
+        XCTAssertEqual(Int.max.addingOrNilOnOverflow(2), nil)
+
+        XCTAssertEqual(Int.max +? -2, Int.max - 2)
+        XCTAssertEqual(Int.max +? -1, Int.max - 1)
+        XCTAssertEqual(Int.max +? 0, Int.max)
+        XCTAssertEqual(Int.max +? 1, nil)
+        XCTAssertEqual(Int.max +? 2, nil)
+    }
+
+    func testDividedOrNilOnOverflow() {
+        XCTAssertEqual(Int.max.dividedOrNilOnOverflow(by: -2), -Int.max / 2)
+        XCTAssertEqual(Int.max.dividedOrNilOnOverflow(by: -1), -Int.max)
+        XCTAssertEqual(Int.max.dividedOrNilOnOverflow(by: 0), nil)
+        XCTAssertEqual(Int.max.dividedOrNilOnOverflow(by: 1), Int.max)
+        XCTAssertEqual(Int.max.dividedOrNilOnOverflow(by: 2), Int.max / 2)
+
+        XCTAssertEqual(Int.max /? -2, -Int.max / 2)
+        XCTAssertEqual(Int.max /? -1, -Int.max)
+        XCTAssertEqual(Int.max /? 0, nil)
+        XCTAssertEqual(Int.max /? 1, Int.max)
+        XCTAssertEqual(Int.max /? 2, Int.max / 2)
+    }
+
+    func testMultipliedOrNilOnOverflow() {
+        XCTAssertEqual(Int.max.multipliedOrNilOnOverflow(by: -2), nil)
+        XCTAssertEqual(Int.max.multipliedOrNilOnOverflow(by: -1), -Int.max)
+        XCTAssertEqual(Int.max.multipliedOrNilOnOverflow(by: 0), 0)
+        XCTAssertEqual(Int.max.multipliedOrNilOnOverflow(by: 1), Int.max)
+        XCTAssertEqual(Int.max.multipliedOrNilOnOverflow(by: 2), nil)
+
+        XCTAssertEqual(Int.max *? -2, nil)
+        XCTAssertEqual(Int.max *? -1, -Int.max)
+        XCTAssertEqual(Int.max *? 0, 0)
+        XCTAssertEqual(Int.max *? 1, Int.max)
+        XCTAssertEqual(Int.max *? 2, nil)
+    }
+
+    func testRemainderOrNilOnOverflow() {
+        XCTAssertEqual(Int.max.remainderOrNilOnOverflow(dividingBy: -2), 1)
+        XCTAssertEqual(Int.max.remainderOrNilOnOverflow(dividingBy: -1), 0)
+        XCTAssertEqual(Int.max.remainderOrNilOnOverflow(dividingBy: 0), nil)
+        XCTAssertEqual(Int.max.remainderOrNilOnOverflow(dividingBy: 1), 0)
+        XCTAssertEqual(Int.max.remainderOrNilOnOverflow(dividingBy: 2), 1)
+    }
+
+    func testSubtractingOrNilOnOverflow() {
+        XCTAssertEqual(Int.max.subtractingOrNilOnOverflow(-2), nil)
+        XCTAssertEqual(Int.max.subtractingOrNilOnOverflow(-1), nil)
+        XCTAssertEqual(Int.max.subtractingOrNilOnOverflow(0), Int.max)
+        XCTAssertEqual(Int.max.subtractingOrNilOnOverflow(1), Int.max - 1)
+        XCTAssertEqual(Int.max.subtractingOrNilOnOverflow(2), Int.max - 2)
+
+        XCTAssertEqual(Int.max -? -2, nil)
+        XCTAssertEqual(Int.max -? -1, nil)
+        XCTAssertEqual(Int.max -? 0, Int.max)
+        XCTAssertEqual(Int.max -? 1, Int.max - 1)
+        XCTAssertEqual(Int.max -? 2, Int.max - 2)
+    }
+}
