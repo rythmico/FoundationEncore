de2ce95a497697c60782b685ae08137c0bec3a67
davdroman <d@vidroman.dev>
2021-10-29 15:03:07 +0100

    Split up Foundation support into separate files

---
+++ Sources/DateTimeOnly/TimeOnly+FoundationSupport.swift
@@ -0,0 +1,30 @@
+import Foundation
+
+extension Date {
+    public init(_ timeOnly: TimeOnly, timeZone: TimeZone) {
+        var calendar = calendar
+        calendar.timeZone = timeZone
+        var dateComponents = DateComponents(timeOnly)
+        dateComponents.calendar = calendar
+        dateComponents.timeZone = timeZone
+        guard let date = dateComponents.date else {
+            preconditionFailure("`DateComponents.date` returned nil")
+        }
+        self = date
+    }
+}
+
+extension TimeOnly {
+    public var asDateComponents: DateComponents {
+        DateComponents(self)
+    }
+}
+
+extension DateComponents {
+    public init(_ timeOnly: TimeOnly) {
+        self.init(
+            hour: timeOnly.hour,
+            minute: timeOnly.minute
+        )
+    }
+}
