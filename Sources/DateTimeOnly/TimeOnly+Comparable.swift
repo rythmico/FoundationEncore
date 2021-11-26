a5be40a47c7f586482ef5383ee533e2f9b1bbe8d
davdroman <d@vidroman.dev>
2021-11-26 18:19:20 +0100

    Simplify `Comparable` conformance for `DateTimeOnly`

--- Sources/DateTimeOnly/TimeOnly+Comparable.swift
+++ Sources/DateTimeOnly/TimeOnly+Comparable.swift
@@ -2,12 +2,6 @@ import Foundation
 
 extension TimeOnly: Comparable {
     public static func < (lhs: Self, rhs: Self) -> Bool {
-        guard lhs.hour == rhs.hour else {
-            return lhs.hour < rhs.hour
-        }
-        guard lhs.minute == rhs.minute else {
-            return lhs.minute < rhs.minute
-        }
-        return false
+        (lhs.hour, lhs.minute) < (rhs.hour, rhs.minute)
     }
 }
