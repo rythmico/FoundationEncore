20fe2edc2786f608d7964eeea7c2797c64d49666
David Roman <d@vidroman.dev>
2022-03-28 16:45:56 +0100

    Introduce `PhoneNumber.formatted` function

--- Sources/FoundationEncore/PhoneNumberEncore.swift
+++ Sources/FoundationEncore/PhoneNumberEncore.swift
@@ -1,3 +1,9 @@
 extension PhoneNumberKit {
     public static let shared = PhoneNumberKit()
 }
+
+extension PhoneNumber {
+    public func formatted(_ format: PhoneNumberFormat) -> String {
+        PhoneNumberKit.shared.format(self, toType: format)
+    }
+}
---
+++ Tests/FoundationEncoreTests/PhoneNumberEncoreTests.swift
@@ -0,0 +1,27 @@
+import FoundationEncore
+@testable import PhoneNumberKit
+import XCTest
+
+final class PhoneNumberEncoreTests: XCTestCase {
+    func testFormatted_ukPhoneNumber() {
+        let sut = PhoneNumber(
+            numberString: "+441632960015",
+            countryCode: 44,
+            leadingZero: false,
+            nationalNumber: 1632960015,
+            numberExtension: nil,
+            type: .unknown,
+            regionID: "GB"
+        )
+        XCTAssertEqual(sut.formatted(.e164), "+441632960015")
+        XCTAssertEqual(sut.formatted(.national), "01632 960015")
+        XCTAssertEqual(sut.formatted(.international), "+44 1632 960015")
+    }
+
+    func testFormatted_notPhoneNumber() {
+        let sut = PhoneNumber.notPhoneNumber()
+        XCTAssertEqual(sut.formatted(.e164), "+00")
+        XCTAssertEqual(sut.formatted(.national), "0")
+        XCTAssertEqual(sut.formatted(.international), "+0 0")
+    }
+}
