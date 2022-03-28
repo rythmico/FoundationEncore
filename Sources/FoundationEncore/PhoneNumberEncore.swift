2b91023a5d748216f64a943479dc2120e0d87820
David Roman <d@vidroman.dev>
2022-03-28 18:39:05 +0100

    Introduce convenience `PhoneNumber` initializers

--- Sources/FoundationEncore/PhoneNumberEncore.swift
+++ Sources/FoundationEncore/PhoneNumberEncore.swift
@@ -5,6 +5,18 @@ extension PhoneNumberKit {
 }
 
 extension PhoneNumber {
+    public init(e164 value: String) throws {
+        self = try PhoneNumberKit.shared.parse(value, withRegion: "US", ignoreType: true)
+    }
+}
+
+extension PhoneNumber: ExpressibleByStringLiteral {
+    public init(stringLiteral value: String) {
+        self = try! PhoneNumber(e164: value)
+    }
+}
+
+extension PhoneNumber {
     public func formatted(_ format: PhoneNumberFormat) -> String {
         PhoneNumberKit.shared.format(self, toType: format)
     }
--- Tests/FoundationEncoreTests/PhoneNumberEncoreTests.swift
+++ Tests/FoundationEncoreTests/PhoneNumberEncoreTests.swift
@@ -1,18 +1,39 @@
 import FoundationEncore
 @testable import struct PhoneNumberKit.PhoneNumber // imports internal PhoneNumber.init
+import enum PhoneNumberKit.PhoneNumberError
 import XCTest
 
 final class PhoneNumberEncoreTests: XCTestCase {
+    let sut = PhoneNumber(
+        numberString: "+441632960015",
+        countryCode: 44,
+        leadingZero: false,
+        nationalNumber: 1632960015,
+        numberExtension: nil,
+        type: .unknown,
+        regionID: "GB"
+    )
+
+    func testInitWithE164_ukPhoneNumber() {
+        XCTAssertEqual(sut, try PhoneNumber(e164: "+441632960015"))
+    }
+
+    func testInitWithStringLiteral_ukPhoneNumber() {
+        XCTAssertEqual(sut, "+441632960015")
+    }
+
+    func testInitWithE164_invalidPhoneNumber() {
+        XCTAssertThrowsError(try PhoneNumber(e164: "+00")) { error in
+            switch error {
+            case PhoneNumberError.notANumber:
+                break
+            default:
+                XCTFail(error.localizedDescription)
+            }
+        }
+    }
+
     func testFormatted_ukPhoneNumber() {
-        let sut = PhoneNumber(
-            numberString: "+441632960015",
-            countryCode: 44,
-            leadingZero: false,
-            nationalNumber: 1632960015,
-            numberExtension: nil,
-            type: .unknown,
-            regionID: "GB"
-        )
         XCTAssertEqual(sut.formatted(.e164), "+441632960015")
         XCTAssertEqual(sut.formatted(.national), "01632 960015")
         XCTAssertEqual(sut.formatted(.international), "+44 1632 960015")
